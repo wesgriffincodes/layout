c2b019fffdd485c2c32280a45b53e0f3eb0aabde
codingclueless <codingclueless@gmail.com>
2019-08-15 16:19:30 -0700

    figlet with this.state

--- src/components/TextFormatter.js
+++ src/components/TextFormatter.js
@@ -6,13 +6,25 @@ class TextFormatter extends Component {
     state = {
       text: 'this.state',
       color: 'black',
+      formattedText: '',
+      font: ''
+    }
+
+    formatText = () => {
+      const { text, font } = this.state;
+      figlet.text(text, {
+        font: font
+      }, (err, result) => {
+        this.setState({ formattedText: result });
+      });
     }
 
 
-
 handleChange = ({ target }) => {
-  this.setState({ [target.name]: target.value });
-}
+  this.setState({ [target.name]: target.value }, () => {
+    this.formatText();
+  });
+};
 
 
 
@@ -21,15 +33,21 @@ render() {
   const {
     text,
     color,
+    formattedText,
+    font
   } = this.state;
 
-
+  const fontOptions = ['Avatar', 'Banner', 'Alligator', 'Linux'].map(font => {
+    return <option key={font} value={font}>{font}</option>;
+  });
 
   return (
     <div>
+      <select name="font" value={font} onChange={this.handleChange}>{fontOptions}</select>
       <input type="text" name="text" value={text} onChange={this.handleChange}/>
       <input type="color" name="color" value={color} onChange={this.handleChange}/>
       <h1 style={{ color: color }}>{text}</h1>
+      <pre style={{ color }}>{formattedText}</pre>
     </div>
   );
 }
